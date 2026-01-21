9490cfd10a332e280a2298e95b62bdeb8a9b0bb2
HobaeBang <bhb0407@naver.com>
2026-01-22 08:01:33 +0900

    feat(student): 전체 학생 목록 조회 API 구현
    - StudentRepository: findAllByAcademy 쿼리 추가
    - StudentService: getAllStudents 메서드 구현
    - StudentController: GET /api/v1/students 추가

--- src/main/java/com/aslan/academymanagement/repository/StudentRepository.java
+++ src/main/java/com/aslan/academymanagement/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.aslan.academymanagement.repository;
 
 
+import com.aslan.academymanagement.domain.Academy;
 import com.aslan.academymanagement.domain.Student;
 import com.aslan.academymanagement.domain.enums.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,16 +15,12 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Optional<Student> findByStudentNumber(String studentNumber);
 
-    // Division 필드가 삭제되었으므로 관련 메서드 제거
-    // List<Student> findByDivision(Division division);
-
     List<Student> findByGrade(Grade grade);
 
     List<Student> findByNameContaining(String name);
 
-    // Division 필드가 삭제되었으므로 쿼리 수정 필요 (일단 제거)
-    // @Query("SELECT s FROM Student s WHERE s.division = :division AND s.grade = :grade")
-    // List<Student> findByDivisionAndGrade(...)
-
     boolean existsByStudentNumber(String studentNumber);
+
+    // 학원별 학생 목록 조회
+    List<Student> findAllByAcademy(Academy academy);
 }
